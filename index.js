9d4f0decde6cc1541e883b8c06c7b4ded6ffecbc
Mathieu <mathieu.linder.dev@gmail.com>
2021-09-13 19:45:31 +0200

    CSS grid, responsive et boutton

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const affichageBd = () => {
 
     const affichageDesAlbums = document.getElementById("bd-container");
     const monImg = (serie.nom + "-" + album.numero + "-" + album.titre).replace(/'|!|\?|\.|"|:|\$/g, "");
-    const bdInsert = "<div class=un-album>" + "<div class='info'>" + "<div class='serie'>" + "Série : " +  serie.nom + "</div>" + "<div class='album'>" + "Album n° : " + album.numero + "</div>" + "<div class='titre'>" + "Titre : "+ album.titre + "</div>" + "<div class='auteur'>" + "Auteur : " + auteur.nom + "</div>" + "<div class='prix'>" + "Prix : " + album.prix  + " €" + "</div>" + "</div>" + '<img class="miniImg" src="./assets/albumsMini/' + monImg + ".jpg" + '"' + ">" + '<input type="button" id="ajoutPanier" name=' + key + ' value="Ajoutez au panier">' + "</div>";
+    const bdInsert = "<div class=un-album>" + "<div class='info'>" + "<div class='serie'>" + serie.nom + "</div>" + "<div class='album'>" + "Album n° : " + album.numero + "</div>" + "<div class='titre'>" + "Titre : "+ album.titre + "</div>" + "<div class='auteur'>" + "Auteur : " + auteur.nom + "</div>" + "<div class='prix'>" + album.prix  + " €" + "</div>" + "</div>" + '<img class="miniImg" src="./assets/albumsMini/' + monImg + ".jpg" + '"' + ">" + '<input type="button" id="ajoutPanier" name=' + key + ' value="">' + "</div>";
     //     affichageDesAlbums.innerHTML += "<div class=unAlbum>" + "<div class='serie'>" + "Série : ..."
     // InnerHtml a des performances beaucoup moins bonnes que inserAdjacentHTML ??     ///     edit: Oui !!    ///
     
@@ -17,4 +17,6 @@ const affichageBd = () => {
 };
 
 document.addEventListener("DOMContentLoaded", affichageBd());
+
+
 
