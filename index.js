2bee0511850421e0ce39b9d24ed9e81c9447a8ce
Mathieu <mathieu.linder.dev@gmail.com>
2021-09-15 21:15:48 +0200

    début de la gallerie dynamique

--- index.js
+++ index.js
@@ -7,16 +7,35 @@ const affichageBd = () => {
 
     const affichageDesAlbums = document.getElementById("bd-container");
     const monImg = (serie.nom + "-" + album.numero + "-" + album.titre).replace(/'|!|\?|\.|"|:|\$/g, "");
-    const bdInsert = "<div class=un-album>" + "<div class='info'>" + "<div class='serie'>" + serie.nom + "</div>" + "<div class='album'>" + "Album n° : " + album.numero + "</div>" + "<div class='titre'>" + "Titre : "+ album.titre + "</div>" + "<div class='auteur'>" + "Auteur : " + auteur.nom + "</div>" + "<div class='prix'>" + album.prix  + " €" + "</div>" + "</div>" + '<img class="miniImg" src="./assets/albumsMini/' + monImg + ".jpg" + '"' + ">" + '<input type="button" id="ajoutPanier" name=' + key + ' value="">' + "</div>";
+    const infoSerie = "<div class='serie'>" + serie.nom + "</div>";
+    const infoAlbum = "<div class='album'>" + "Album n° : " + album.numero + "</div>";
+    const infoTitre = "<div class='titre'>" + "Titre : "+ album.titre + "</div>";
+    const infoAuteur = "<div class='auteur'>" + "Auteur : " + auteur.nom + "</div>";
+    const infoPrix = "<div class='prix'>" + album.prix  + " €" + "</div>";
+    const imgClass = '<img class="miniImg" id= ' + key + ' src="./assets/albumsMini/' + monImg + ".jpg" + '"' + ">";
+    const btnKey = '<input type="button" id="ajoutPanier" name=' + key + ' value="">';
+
+    const bdInsert = "<div class=un-album>" + "<div class='info'>" + infoSerie + infoAlbum + infoTitre + infoAuteur + infoPrix + "</div>" + imgClass + btnKey + "</div>";
     //     affichageDesAlbums.innerHTML += "<div class=unAlbum>" + "<div class='serie'>" + "Série : ..."
     // InnerHtml a des performances beaucoup moins bonnes que inserAdjacentHTML ??     ///     edit: Oui !!    ///
     
     affichageDesAlbums.insertAdjacentHTML("beforeend", bdInsert);
-
-    })
+    });
 };
 
 document.addEventListener("DOMContentLoaded", affichageBd());
 
 
+const gallery = () => {
+    albums.forEach((album, key) => {
+        serie = series.get(album.idSerie);
 
+        const gallery = document.getElementById('carousel');
+        const monImg = (serie.nom + "-" + album.numero + "-" + album.titre).replace(/'|!|\?|\.|"|:|\$/g, "");
+        const imgGallery = '<img class="miniImg" id= ' + key + ' src="./assets/albumsMini/' + monImg + ".jpg" + '"' + ">";
+
+        gallery.insertAdjacentHTML("beforeend", imgGallery);
+    });
+};
+
+document.addEventListener("DOMContentLoad", gallery());
